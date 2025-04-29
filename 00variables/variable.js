7907f8bbb6962deb4a634eb60bade8bfda7011c1
sushmitha-cs <sushmithacs30@gmail.com>
2025-04-28 21:56:11 -0500

    Examples of variables

---
+++ 00variables/variable.js
@@ -0,0 +1,16 @@
+// Declare a variable and print it to the console
+let developer = "Sushmitha Pranav";
+console.log(developer);
+
+// Reassign the variable and print it to the console
+developer = "Pranav";
+console.log(developer);
+
+// Unintialized variables
+let variableOne;
+let secondVariable;
+console.log(variableOne);
+
+//Declare array and print it to the console
+let array = ["first", "second"];
+console.log(array);
