c5993d23633fdb0d76fa5a8305b8ad4e2e3f28eb
sushmitha-cs <sushmithacs30@gmail.com>
2025-04-28 22:44:44 -0500

    Reassigning the array value

--- 00variables/variable.js
+++ 00variables/variable.js
@@ -14,3 +14,8 @@ console.log(variableOne);
 //Declare array and print it to the console
 let array = ["first", "second"];
 console.log(array);
+
+// Reassign the array and print it to the console
+let array1 = [1, 2, 3];
+array1[1] = 25;
+console.log(array1); // prints [1, 25, 3]
