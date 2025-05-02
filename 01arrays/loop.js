cca41caf4ad689372d1fefdaa68677910eb8eeec
sushmitha-cs <sushmithacs30@gmail.com>
2025-05-02 16:09:44 -0500

    for of loop suntax and example

--- 01arrays/loop.js
+++ 01arrays/loop.js
@@ -3,3 +3,11 @@
 for (let i = 0; i < 5; i++) {
     console.log(i); // prints 0 to 4
 }
+
+//syntax for of loop
+// for (const value of iterable) {}
+let array = [1, 2, 3, 4, 5];    
+for (let element of array) {
+    console.log(element); // prints 1 to 5
+}
+
