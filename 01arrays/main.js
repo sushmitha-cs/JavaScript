253d47f0bf03d914c163d2dcd98deefdef2c41b9
sushmitha-cs <sushmithacs30@gmail.com>
2025-05-15 22:37:32 -0500

    pop, shift, unshift code updated

--- 01arrays/main.js
+++ 01arrays/main.js
@@ -3,6 +3,23 @@
 let array = [1, 2, 3, 4, 5];
 console.log(array.length);
 
-// push the array to the end of the array
+// push the array element to the end of the array
 array.push(6);
 console.log(array);
+console.log(array.length)
+
+// removes the array element from the end of the array
+array.pop()
+console.log(array);
+console.log(array.length)
+
+// adds the array element at the beginning of the array
+array.unshift(0);
+console.log(array); 
+console.log(array.length); 
+
+// removes the array element from the beginning of the array
+array.shift();
+console.log(array); 
+console.log(array.length); 
+
